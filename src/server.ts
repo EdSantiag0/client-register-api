9cca4eacc26caa358edb89460df2984dc899bbb9
Eduardo Santiago <eduardosantiago_@hotmail.com>
2025-07-03 10:31:58 -0300

    fix: server.ts cors change

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import Fastify from "fastify";
-import { routes } from "./routes";
 import cors from "@fastify/cors";
+import { routes } from "./routes";
 
 const app = Fastify({ logger: true });
 const port = Number(process.env.PORT) || 3333;
@@ -9,27 +9,34 @@ app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message });
 });
 
+app.register(cors, {
+  origin: (origin, cb) => {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:3333",
+      "https://client-register-frontend.vercel.app",
+    ];
+
+    if (!origin || allowedOrigins.includes(origin)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"), false);
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+});
+
+app.register(routes);
+
 const start = async () => {
-  await app.register(cors, {
-    origin: (origin, cb) => {
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "http://localhost:3333",
-        "https://client-register-frontend.vercel.app",
-      ];
-
-      if (!origin || allowedOrigins.includes(origin)) {
-        cb(null, true);
-      } else {
-        cb(new Error("Not allowed by CORS"), false);
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  });
-
-  await app.register(routes);
-  await app.listen({ port, host: "0.0.0.0" });
+  try {
+    await app.listen({ port, host: "0.0.0.0" });
+    console.log(`Server is running on port ${port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 };
 
 start();
