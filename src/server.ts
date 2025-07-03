610f9a27c30a3fb65bc6eb749b09f012c9e937b1
Eduardo Santiago <eduardosantiago_@hotmail.com>
2025-07-03 15:19:55 -0300

    fix: server.ts mudando a ordem de execução

--- src/server.ts
+++ src/server.ts
@@ -9,43 +9,34 @@ app.setErrorHandler((error, request, reply) => {
   reply.code(400).send({ message: error.message });
 });
 
-app.register(cors, {
-  origin: (origin, cb) => {
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "http://localhost:3333",
-      "https://client-register-frontend.vercel.app",
-    ];
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      cb(null, true);
-    } else {
-      cb(new Error("Not allowed by CORS"), false);
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-});
-
-app.register(routes);
-
 const start = async () => {
   try {
+    await app.register(cors, {
+      origin: (origin, cb) => {
+        const allowedOrigins = [
+          "http://localhost:5173",
+          "http://localhost:3333",
+          "https://client-register-frontend.vercel.app",
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+          cb(null, true);
+        } else {
+          cb(new Error("Not allowed by CORS"), false);
+        }
+      },
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
+    });
+
+    await app.register(routes);
+
     await app.listen({ port, host: "0.0.0.0" });
-    console.log(`Server is running on port ${port}`);
+    console.log(`🚀 Server running on port ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
   }
 };
-
-app.get("/ping", async () => {
-  return { pong: true };
-});
-
-fetch("https://client-register-api-czmx.onrender.com/ping")
-  .then((res) => res.json())
-  .then(console.log)
-  .catch(console.error);
 
 start();
